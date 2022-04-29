7cb601a9c1a27ee94b65cdefb5b20b852f316c27
zhangjun <ibmzhangjun@139.com>
2022-04-29 13:09:50 +0800

    Init Project

--- utils/adminui/__init__.py
+++
@@ -1,9 +0,0 @@
-from .form import *
-from .page import *
-from .layout import *
-from .table import *
-from .actions import *
-from .chart import *
-from .app import AdminApp, MenuItem, LoggedInUser, LoginFailed
-from flask import Flask, jsonify
-
