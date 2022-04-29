860c789e778846cc192c271088496da25fae24b2
zhangjun <ibmzhangjun@139.com>
2022-04-29 20:41:17 +0800

    Init Project

--- apps/__init__.py
+++ apps/__init__.py
@@ -28,7 +28,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home', 'ossgov'):
+    for module_name in ('authentication', 'home', 'ossgov', 'govass', 'sysdev', 'sysadmin'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
