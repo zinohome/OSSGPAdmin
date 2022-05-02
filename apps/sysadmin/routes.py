8d6cc3b3a91f2864506698b341dc9a5ced6f86b0
zhangjun <ibmzhangjun@139.com>
2022-05-02 08:05:08 +0800

    Init Project

--- apps/sysadmin/routes.py
+++ apps/sysadmin/routes.py
@@ -27,35 +27,29 @@ def route_sysadmin():
                            startdate=config('OSSGPADMIN_SYS_START_DAY', default='2020-02-19'),
                            today=today)
 
+
 @blueprint.route('/sysadmin-users.html', methods = ['GET', 'POST'])
 @login_required
 def route_sysadmin_users():
+    log.logger.debug(request.args)
     if oc.token_expired:
         oc.renew_token()
     today = time.strftime("%Y-%m-%d", time.localtime())
-    pageinfo = {}
-    pageinfo['rcount'] = oc.fetchcount('users')['body']
-    pageinfo['rpagesize'] = config('OSSGPADMIN_SYS_PAGE_SIZE', default=10, cast=int)
-    pageinfo['rpages'] = pageinfo['rcount'] // pageinfo['rpagesize'] + 1
-    pageinfo['curpage'] = 1
     definestr = oc.fetch('users', '_sysdef', None, 0, 5)['body']
+    count = oc.fetchcount('users')['body']
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    perpage = config('OSSGPADMIN_SYS_PAGE_SIZE', default=10, cast=int)
     define = {}
     define['colname'] = definestr['data'][0]['name']
     define['keyfieldname'] = definestr['data'][0]['keyfieldname']
     define['coldef'] = json.loads(definestr['data'][0]['coldef'])
-    record = oc.fetch('users', '_collection', None, (pageinfo['curpage']-1)*pageinfo['rpagesize'], pageinfo['rpagesize'])['body']
+    record = oc.fetch('users', '_collection', None, (page-1)*perpage, perpage)['body']
     log.logger.debug(definestr)
     log.logger.debug(define)
     log.logger.debug(record)
-    log.logger.debug(pageinfo)
-    page = request.args.get(get_page_parameter(), type=int, default=1)
-
-    pagination = Pagination(page=page, total=pageinfo['rcount'], search=False,
-                            record_name='record', page_parameter='p',
-                            format_total=True, format_number=True)
+    pagination = Pagination(page=page, per_page=perpage, total=count, search=False)
     log.logger.debug('pagination: %s' % pagination.links)
-    #log.logger.debug(page)
     return render_template('sysadmin/sysadmin-users.html', segment='sysadmin-users',
-                           define = define, record = record, pageinfo = pageinfo, pagination=pagination,
+                           define = define, record = record, pagination=pagination,
                            startdate=config('OSSGPADMIN_SYS_START_DAY', default='2020-02-19'),
                            today=today)
