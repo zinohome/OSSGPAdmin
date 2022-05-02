865715ae84ddc028f9764bee4c39d94883d9902c
zhangjun <ibmzhangjun@139.com>
2022-05-02 08:22:36 +0800

    Init Project

--- apps/sysadmin/routes.py
+++ apps/sysadmin/routes.py
@@ -27,7 +27,7 @@ def route_sysadmin():
                            startdate=config('OSSGPADMIN_SYS_START_DAY', default='2020-02-19'),
                            today=today)
 
-@blueprint.route('/sysadmin-users.html', methods = ['GET', 'POST', 'PUT', 'DELETE'])
+@blueprint.route('/sysadmin-users.html', methods = ['GET', 'POST'])
 @login_required
 def route_sysadmin_users():
     if request.method == 'GET':
@@ -51,3 +51,32 @@ def route_sysadmin_users():
                                define = define, record = record, pagination=pagination,
                                startdate=config('OSSGPADMIN_SYS_START_DAY', default='2020-02-19'),
                                today=today)
+    elif request.method == 'POST':
+        pass
+
+@blueprint.route('/sysadmin-authority.html', methods = ['GET', 'POST'])
+@login_required
+def route_sysadmin_authority():
+    if request.method == 'GET':
+        if oc.token_expired:
+            oc.renew_token()
+        today = time.strftime("%Y-%m-%d", time.localtime())
+        definestr = oc.fetch('users', '_sysdef', None, 0, 5)['body']
+        count = oc.fetchcount('users')['body']
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        perpage = config('OSSGPADMIN_SYS_PAGE_SIZE', default=10, cast=int)
+        define = {}
+        define['colname'] = definestr['data'][0]['name']
+        define['keyfieldname'] = definestr['data'][0]['keyfieldname']
+        define['coldef'] = json.loads(definestr['data'][0]['coldef'])
+        record = oc.fetch('users', '_collection', None, (page-1)*perpage, perpage)['body']
+        #log.logger.debug(definestr)
+        #log.logger.debug(define)
+        #log.logger.debug(record)
+        pagination = Pagination(page=page, per_page=perpage, total=count, search=False)
+        return render_template('sysadmin/sysadmin-authority.html', segment='sysadmin-authority',
+                               define = define, record = record, pagination=pagination,
+                               startdate=config('OSSGPADMIN_SYS_START_DAY', default='2020-02-19'),
+                               today=today)
+    elif request.method == 'POST':
+        pass
