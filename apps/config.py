9ca835b30a97a67e6eb16415a2d4b0c944e92a33
zhangjun <ibmzhangjun@139.com>
2022-05-05 22:38:13 +0800

    user admin crud

--- apps/config.py
+++ apps/config.py
@@ -34,18 +34,13 @@ class ProductionConfig(Config):
     SESSION_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
-    # PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-        config('DB_ENGINE', default='postgresql'),
-        config('DB_USERNAME', default='appseed'),
-        config('DB_PASS', default='pass'),
-        config('DB_HOST', default='localhost'),
-        config('DB_PORT', default=5432),
-        config('DB_NAME', default='appseed-flask')
-    )
 
 class DebugConfig(Config):
     DEBUG = True
+    # Security
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_DURATION = 3600
 
 # Load all possible configurations
 config_dict = {
