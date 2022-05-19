e6723aa15d8c53e22a69a00925a113a09ba05bc1
zhangjun <ibmzhangjun@139.com>
2022-05-19 09:21:03 +0800

    add flask-cach and resolve cache problem

--- apps/__init__.py
+++ apps/__init__.py
@@ -30,7 +30,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home', 'ossgov', 'govass', 'sysdev', 'sysadmin', 'jsontool'):
+    for module_name in ('authentication', 'home', 'ossgov', 'govass', 'sysdev', 'sysadmin', 'jsontool', 'syshelp'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
---
+++ apps/syshelp/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  #
+#  Copyright (C) 2022 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2022
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: OSSGPAdmin
+
+from flask import Blueprint
+
+blueprint = Blueprint(
+    'syshelp_blueprint',
+    __name__,
+    url_prefix=''
+)
